8db6a4550aa82c99052157b386eb5977956564f5
Damien Luzzo <damienluzzo33@gmail.com>
2022-03-14 21:46:24 -0500

    fix bug so that data displays in proper textbox on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,25 @@ var currentDate = $('#currentDay');
 // grab the reset button
 var resetButton = $('#reset-btn');
 // crate global variables
-var timeBox, hourOfDay, scheduleInput, saveButton, hourSpan, saveIcon, date, currentTime;
+var timeBox, hourOfDay, scheduleInput, saveButton, hourSpan, saveIcon, date, currentTime, scheduleArray;
+
+// create a function that can be used to extract memory when page is reloaded or revisited
+function extractAndDisplay() {
+    // fetch the data from local storage and parse it
+    let savedScheduleData = JSON.parse(localStorage.getItem("scheduleData"));
+
+    if (!savedScheduleData) scheduleArray = new Array(19).fill("");
+    else scheduleArray = savedScheduleData;
+    // loop through the time boxes
+    for (var i = 0; i < 19; i++){
+        // console.log(savedTextInput);
+        const data = scheduleArray[i];
+        console.log(data);
+        $('.userInput').eq(`${i}`).val(data);
+    }
+}
+
+
 // define a function that calls checkTime and getCurrentTime functions
 function fetchDateAndTime() {
     // use moment.js to generate the current date
@@ -39,17 +57,17 @@ function fetchDateAndTime() {
 fetchDateAndTime();
 setInterval(fetchDateAndTime, 1000);
 // iterate 24 times to create rows of data with 3 total columns
-for (var i = 5; i < 24; i++) {
+for (var i = 0; i < 19; i++) {
 	// create div for the time box rows
-	timeBox = $('<div>', { class: 'input-group input-group-lg row m-0 pr-0 col d-flex justify-content-center', id: `${i + 1}` });
+	timeBox = $('<div>', { class: 'input-group input-group-lg row m-0 pr-0 col d-flex justify-content-center', id: `${i + 6}` });
 	// create div to hold hour display span tag and create span tag
 	hourDiv = $('<div>', { class: 'col-2 row input-group-prepend text-center p-0' });
-	hourSpan = $('<span>', { class: 'col input-group-text d-flex justify-content-center', style: 'font-size: 1rem', id: `${i + 1}` });
+	hourSpan = $('<span>', { class: 'col input-group-text d-flex justify-content-center', style: 'font-size: 1rem', id: `${i + 6}` });
 	// create input text box
-	scheduleInput = $('<textarea>', { class: 'userInput col-8 border-top border-bottom border-left-0', type: 'text', 'aria-label': 'text-box for calendar entry', 'aria-describedby': `schedule-entry-${i + 1}`, id: `${i + 1}` });
+	scheduleInput = $('<textarea>', { class: 'userInput col-8 border-top border-bottom border-left-0', type: 'text', 'aria-label': 'text-box for calendar entry', 'aria-describedby': `schedule-entry-${i + 6}`, id: `${i + 6}` });
 	// create div to hold save button and create button element
 	saveDiv = $('<div>', { class: 'input-group-append col-2 row p-0 m-0' });
-	saveButton = $('<button>', { class: 'btn btn-outline-secondary col saveBtn', type: 'button', 'aria-label': "save button", id: `schedule-entry-${i + 1}` });
+	saveButton = $('<button>', { class: 'btn btn-outline-secondary col saveBtn', type: 'button', 'aria-label': "save button", id: `schedule-entry-${i + 6}` });
 	// create save button icon element and add font awesome class for icon
 	saveIcon = $('<i>', { class: 'fas fa-save' });
 	// append timeBox to the calendar
@@ -58,10 +76,10 @@ for (var i = 5; i < 24; i++) {
 	timeBox.append(hourDiv);
 	hourDiv.append(hourSpan);
     // use conditional logic to set the correct time with AM or PM
-	if (i - 11 < 0) hourSpan.text(`${i + 1} AM`);
-    else if (i - 11 === 0) hourSpan.text('12 PM')
-	else if (i === 23) hourSpan.text(`${i - 11} AM`);
-	else hourSpan.text(`${i - 11} PM`);
+	if (i - 6 < 0) hourSpan.text(`${i + 6} AM`);
+    else if (i - 6 === 0) hourSpan.text('12 PM')
+	else if (i === 18) hourSpan.text(`${i - 6} AM`);
+	else hourSpan.text(`${i - 6} PM`);
 	// append input to the timeBox
 	timeBox.append(scheduleInput);
 	// append save button div to timeBox and append button to div
@@ -78,21 +96,11 @@ function buttonClickHandler() {
     var textAreaText = $(this).parent().siblings().eq(1).val();
     var inputIdKey = $(this).parent().siblings().eq(1).attr('id');
     // console.log(inputIdKey);
+    scheduleArray[inputIdKey - 6] = textAreaText;
     // store textAreaText value in local storage with unique keys
-    localStorage.setItem(inputIdKey, JSON.stringify(textAreaText));
+    localStorage.setItem("scheduleData", JSON.stringify(scheduleArray));
 };
-// create a function that can be used to extract memory when page is reloaded or revisited
-function extractAndDisplay() {
-    // loop through the time boxes
-    for (var i = 5; i < 24; i++){
-        // fetch the data from local storage and parse it
-        var savedTextInput = JSON.parse(localStorage.getItem(`${i+1}`));
-        // console.log(savedTextInput);
-        $('.userInput').eq(`${i}`).val(savedTextInput);
-    }
-}
-// call the function to extract and display previously saved 
-extractAndDisplay();
+
 // create function that will reset the values of all of the text boxes
 function resetValues() {
     localStorage.clear();
@@ -101,5 +109,7 @@ function resetValues() {
     }
     
 }
+// call the function to extract and display previously saved 
+extractAndDisplay();
 // add event listener for reset button
 $(resetButton).on("click", resetValues);
